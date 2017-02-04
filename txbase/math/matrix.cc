a4e1790e5228c519c433e2156c2081d498ba45fe
Tian Xia <cruisky@gmail.com>
2017-02-03 23:06:51 -0500

    fix header inclusion

--- txbase/math/matrix.cc
+++ txbase/math/matrix.cc
@@ -2,6 +2,7 @@
 #include "txbase/math/matrix.h"
 #include "txbase/math/vector.h"
 #include "txbase/math/ray.h"
+#include "txbase/sse/sse.h"
 
 namespace TX{
 	const Matrix3x3 Matrix3x3::IDENTITY = Matrix3x3(
@@ -15,6 +16,89 @@ namespace TX{
 		0.f, 1.f, 0.f, 0.f,
 		0.f, 0.f, 1.f, 0.f,
 		0.f, 0.f, 0.f, 1.f);
+
+	Matrix4x4 Matrix4x4::Inverse() const {
+		Matrix4x4 result;
+		const float *src = (const float *)*row;
+		float *dst = (float *)result[0];
+		// Intel AP-928
+		__m128 minor0, minor1, minor2, minor3;
+		__m128 row0, row1, row2, row3;
+		__m128 det, tmp1;
+		tmp1 = _mm_loadh_pi(_mm_loadl_pi(_mm_setzero_ps(), (__m64*)(src)), (__m64*)(src + 4));
+		row1 = _mm_loadh_pi(_mm_loadl_pi(_mm_setzero_ps(), (__m64*)(src + 8)), (__m64*)(src + 12));
+		row0 = _mm_shuffle_ps(tmp1, row1, 0x88);
+		row1 = _mm_shuffle_ps(row1, tmp1, 0xDD);
+		tmp1 = _mm_loadh_pi(_mm_loadl_pi(tmp1, (__m64*)(src + 2)), (__m64*)(src + 6));
+		row3 = _mm_loadh_pi(_mm_loadl_pi(_mm_setzero_ps(), (__m64*)(src + 10)), (__m64*)(src + 14));
+		row2 = _mm_shuffle_ps(tmp1, row3, 0x88);
+		row3 = _mm_shuffle_ps(row3, tmp1, 0xDD);					//
+		tmp1 = _mm_mul_ps(row2, row3);
+		tmp1 = _mm_shuffle_ps(tmp1, tmp1, 0xB1);
+		minor0 = _mm_mul_ps(row1, tmp1);
+		minor1 = _mm_mul_ps(row0, tmp1);
+		tmp1 = _mm_shuffle_ps(tmp1, tmp1, 0x4E);
+		minor0 = _mm_sub_ps(_mm_mul_ps(row1, tmp1), minor0);
+		minor1 = _mm_sub_ps(_mm_mul_ps(row0, tmp1), minor1);
+		minor1 = _mm_shuffle_ps(minor1, minor1, 0x4E);				//
+		tmp1 = _mm_mul_ps(row1, row2);
+		tmp1 = _mm_shuffle_ps(tmp1, tmp1, 0xB1);
+		minor0 = _mm_add_ps(_mm_mul_ps(row3, tmp1), minor0);
+		minor3 = _mm_mul_ps(row0, tmp1);
+		tmp1 = _mm_shuffle_ps(tmp1, tmp1, 0x4E);
+		minor0 = _mm_sub_ps(minor0, _mm_mul_ps(row3, tmp1));
+		minor3 = _mm_sub_ps(_mm_mul_ps(row0, tmp1), minor3);
+		minor3 = _mm_shuffle_ps(minor3, minor3, 0x4E);				//
+		tmp1 = _mm_mul_ps(_mm_shuffle_ps(row1, row1, 0x4E), row3);
+		tmp1 = _mm_shuffle_ps(tmp1, tmp1, 0xB1);
+		row2 = _mm_shuffle_ps(row2, row2, 0x4E);
+		minor0 = _mm_add_ps(_mm_mul_ps(row2, tmp1), minor0);
+		minor2 = _mm_mul_ps(row0, tmp1);
+		tmp1 = _mm_shuffle_ps(tmp1, tmp1, 0x4E);
+		minor0 = _mm_sub_ps(minor0, _mm_mul_ps(row2, tmp1));
+		minor2 = _mm_sub_ps(_mm_mul_ps(row0, tmp1), minor2);
+		minor2 = _mm_shuffle_ps(minor2, minor2, 0x4E);				//
+		tmp1 = _mm_mul_ps(row0, row1);
+		tmp1 = _mm_shuffle_ps(tmp1, tmp1, 0xB1);
+		minor2 = _mm_add_ps(_mm_mul_ps(row3, tmp1), minor2);
+		minor3 = _mm_sub_ps(_mm_mul_ps(row2, tmp1), minor3);
+		tmp1 = _mm_shuffle_ps(tmp1, tmp1, 0x4E);
+		minor2 = _mm_sub_ps(_mm_mul_ps(row3, tmp1), minor2);
+		minor3 = _mm_sub_ps(minor3, _mm_mul_ps(row2, tmp1));		//
+		tmp1 = _mm_mul_ps(row0, row3);
+		tmp1 = _mm_shuffle_ps(tmp1, tmp1, 0xB1);
+		minor1 = _mm_sub_ps(minor1, _mm_mul_ps(row2, tmp1));
+		minor2 = _mm_add_ps(_mm_mul_ps(row1, tmp1), minor2);
+		tmp1 = _mm_shuffle_ps(tmp1, tmp1, 0x4E);
+		minor1 = _mm_add_ps(_mm_mul_ps(row2, tmp1), minor1);
+		minor2 = _mm_sub_ps(minor2, _mm_mul_ps(row1, tmp1));		//
+		tmp1 = _mm_mul_ps(row0, row2);
+		tmp1 = _mm_shuffle_ps(tmp1, tmp1, 0xB1);
+		minor1 = _mm_add_ps(_mm_mul_ps(row3, tmp1), minor1);
+		minor3 = _mm_sub_ps(minor3, _mm_mul_ps(row1, tmp1));
+		tmp1 = _mm_shuffle_ps(tmp1, tmp1, 0x4E);
+		minor1 = _mm_sub_ps(minor1, _mm_mul_ps(row3, tmp1));
+		minor3 = _mm_add_ps(_mm_mul_ps(row1, tmp1), minor3);		//
+		det = _mm_mul_ps(row0, minor0);
+		det = _mm_add_ps(_mm_shuffle_ps(det, det, 0x4E), det);
+		det = _mm_add_ss(_mm_shuffle_ps(det, det, 0xB1), det);
+		tmp1 = _mm_rcp_ss(det);
+		det = _mm_sub_ss(_mm_add_ss(tmp1, tmp1), _mm_mul_ss(det, _mm_mul_ss(tmp1, tmp1)));
+		det = _mm_shuffle_ps(det, det, 0x00);
+		minor0 = _mm_mul_ps(det, minor0);
+		_mm_storel_pi((__m64*)(dst), minor0);
+		_mm_storeh_pi((__m64*)(dst + 2), minor0);
+		minor1 = _mm_mul_ps(det, minor1);
+		_mm_storel_pi((__m64*)(dst + 4), minor1);
+		_mm_storeh_pi((__m64*)(dst + 6), minor1);
+		minor2 = _mm_mul_ps(det, minor2);
+		_mm_storel_pi((__m64*)(dst + 8), minor2);
+		_mm_storeh_pi((__m64*)(dst + 10), minor2);
+		minor3 = _mm_mul_ps(det, minor3);
+		_mm_storel_pi((__m64*)(dst + 12), minor3);
+		_mm_storeh_pi((__m64*)(dst + 14), minor3);
+		return result;
+	}
 
 	Matrix4x4 Matrix4x4::Translate(const Vec3& v){
 		return Translate(v.x, v.y, v.z);
