6e1e06de93346755830686d69e9e5ab65f5bf53f
Tian Xia <cruisky@gmail.com>
2017-07-25 18:01:48 -0400

    opengl error message retrieval

--- txbase/opengl/opengl.h
+++ txbase/opengl/opengl.h
@@ -45,17 +45,9 @@ namespace TX
 			GLenum code;
 		public:
 			Error(GLenum code): code(code), runtime_error(GetErrorString(code)) {}
-		private:
+		public:
 			static std::string GetErrorString(GLenum code) {
-				switch(code) {
-					case GL_NO_ERROR:						return "GL_NO_ERROR";
-					case GL_INVALID_OPERATION:				return "GL_INVALID_OPERATION";
-					case GL_INVALID_ENUM:					return "GL_INVALID_ENUM";
-					case GL_INVALID_VALUE:					return "GL_INVALID_VALUE";
-					case GL_OUT_OF_MEMORY:					return "GL_OUT_OF_MEMORY";
-					case GL_INVALID_FRAMEBUFFER_OPERATION:	return "GL_INVALID_FRAMEBUFFER_OPERATION";
-					default:								return "Unknown error code";
-				}
+				return (char *) gluErrorString(code);
 			}
 		};
 
